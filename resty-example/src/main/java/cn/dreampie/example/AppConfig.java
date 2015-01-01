b4905c703207f7f6b191b05f1e3b67ad99e56445
wangrenhui <wangrenhui1990@gmail.com>
2015-01-01 19:19:19 +0800

    first  in use

--- resty-example/src/main/java/cn/dreampie/example/AppConfig.java
+++ resty-example/src/main/java/cn/dreampie/example/AppConfig.java
@@ -1,7 +1,11 @@
 package cn.dreampie.example;
 
+import cn.dreampie.orm.ActiveRecordPlugin;
+import cn.dreampie.orm.druid.DruidPlugin;
 import cn.dreampie.route.config.*;
 import cn.dreampie.route.render.JsonRender;
+import cn.dreampie.util.properties.Prop;
+import cn.dreampie.util.properties.Proper;
 
 /**
  * Created by ice on 14-12-29.
@@ -16,7 +20,12 @@ public class AppConfig extends Config {
   }
 
   public void configPlugin(PluginLoader pluginLoader) {
-
+    Prop prop = Proper.use("application.properties");
+    DruidPlugin druidPlugin = new DruidPlugin(prop.get("db.default.url"), prop.get("db.default.user"), prop.get("db.default.password"), prop.get("db.default.driver"), prop.get("db.default.dialect"));
+    pluginLoader.add(druidPlugin);
+    ActiveRecordPlugin activeRecordPlugin = new ActiveRecordPlugin(druidPlugin);
+    activeRecordPlugin.addIncludePaths("cn.dreampie.example");
+    pluginLoader.add(activeRecordPlugin);
   }
 
   public void configInterceptor(InterceptorLoader interceptorLoader) {

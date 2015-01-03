9f319eecabfdd176e7e7b9ecbaf72421658bcca0
wangrenhui <wangrenhui1990@gmail.com>
2015-01-03 19:44:26 +0800

    interceptor  in value

--- resty-example/src/main/java/cn/dreampie/config/AppConfig.java
+++ resty-example/src/main/java/cn/dreampie/config/AppConfig.java
@@ -22,13 +22,14 @@ public class AppConfig extends Config {
 
   public void configPlugin(PluginLoader pluginLoader) {
     Prop prop = Proper.use("application.properties");
+    //第一个数据库
     DruidPlugin druidPlugin = new DruidPlugin(prop.get("db.default.url"), prop.get("db.default.user"), prop.get("db.default.password"), prop.get("db.default.driver"), prop.get("db.default.dialect"));
     pluginLoader.add(druidPlugin);
     ActiveRecordPlugin activeRecordPlugin = new ActiveRecordPlugin("default", druidPlugin);
     activeRecordPlugin.addIncludePaths("cn.dreampie.example");
     activeRecordPlugin.setShowSql(true);
     pluginLoader.add(activeRecordPlugin);
-
+    //第二个数据库
     DruidPlugin demoPlugin = new DruidPlugin(prop.get("db.demo.url"), prop.get("db.demo.user"), prop.get("db.demo.password"), prop.get("db.demo.driver"), prop.get("db.demo.dialect"));
     pluginLoader.add(demoPlugin);
     ActiveRecordPlugin demoRecordPlugin = new ActiveRecordPlugin("demo", demoPlugin);

51c9a8f1100865bf275e7c1389d518618ea7ec42
Dreampie <wangrenhui1990@gmail.com>
2015-03-24 16:14:53 +0800

    change valid to validator

--- resty-example/src/main/java/cn/dreampie/resource/user/UserResource.java
+++ resty-example/src/main/java/cn/dreampie/resource/user/UserResource.java
@@ -9,10 +9,7 @@ import cn.dreampie.resource.user.model.User;
 import cn.dreampie.resource.user.model.UserInfo;
 import cn.dreampie.resource.user.service.UserService;
 import cn.dreampie.resource.user.service.UserServiceImpl;
-import cn.dreampie.route.core.annotation.DELETE;
-import cn.dreampie.route.core.annotation.GET;
-import cn.dreampie.route.core.annotation.POST;
-import cn.dreampie.route.core.annotation.PUT;
+import cn.dreampie.route.core.annotation.*;
 
 import java.util.List;
 import java.util.Set;
@@ -20,6 +17,7 @@ import java.util.Set;
 /**
  * Created by ice on 14-12-29.
  */
+@API("/users")
 public class UserResource extends ApiResource {
   //用于使用service层的 事务
   // @Transaction(name = {"default", "demo"})的注解需要写在service的接口上
@@ -27,45 +25,45 @@ public class UserResource extends ApiResource {
   private UserService userService = AspectFactory.newInstance(new UserServiceImpl(), new TransactionAspect());
 
   //查询集合
-  @GET("/users")
+  @GET
   public List<User> findAll() {
     return User.dao.findAll();
   }
 
   //查询单个user对象
-  @GET("/users/:id")
+  @GET("/:id")
   public User find(String id) {
     return User.dao.findById(id);
   }
 
   //全部对象分页 http://localhost:8081/api/v1.0/users/1/10
-  @GET("/users/:pageNumber/:pageSize")
+  @GET("/:pageNumber/:pageSize")
   public Page<User> paginate(int pageNumber, int pageSize) {
     return User.dao.paginateAll(pageNumber, pageSize);
   }
 
   //按条件分页 http://localhost:8081/api/v1.0/users/1/10/x
-  @GET("/users/:page_Number/:_page$Size/:$term")
+  @GET("/:page_Number/:_page$Size/:$term")
   public Page<User> paginate(int page_Number, int _page$Size, String $term) {
     return User.dao.paginateBy(page_Number, _page$Size, "username=?", $term);
   }
 
   //更新
-  @PUT("/users")
+  @PUT
   public User put(User user) {
     user.update();
     return user;
   }
 
   //保存
-  @POST("/users/:id")
+  @POST("/:id")
   public Set<User> save(int id, Set<User> users) {
     userService.save(users.iterator().next());
     return users;
   }
 
   //删除
-  @DELETE("/users/:id")
+  @DELETE("/:id")
   public boolean put(String id) {
     return User.dao.deleteById(id);
   }

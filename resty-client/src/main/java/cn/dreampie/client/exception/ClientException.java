d12b6f94f6ce0b2bb6454f9e33e43e4eb27f639a
Dreampie <Dreampie@outlook.com>
2015-05-12 13:22:01 +0800

    add file test and fix  route  no params

--- resty-client/src/main/java/cn/dreampie/client/exception/ClientException.java
+++ resty-client/src/main/java/cn/dreampie/client/exception/ClientException.java
@@ -1,12 +1,9 @@
 package cn.dreampie.client.exception;
 
-import cn.dreampie.common.http.result.HttpStatus;
-
 /**
  * ClientException
  */
 public class ClientException extends RuntimeException {
-  private HttpStatus status;
 
   /**
    * Constructor.
@@ -42,57 +39,6 @@ public class ClientException extends RuntimeException {
     super(message, cause);
   }
 
-
-  /**
-   * Constructor.
-   *
-   * @param status It's usually a HTTP Status Code (404, 500, etc.)
-   */
-  public ClientException(HttpStatus status) {
-    super();
-    this.status = status;
-  }
-
-  /**
-   * Constructor.
-   *
-   * @param message Exception message.
-   * @param cause   An exception.
-   * @param status  It's usually a HTTP Status Code (404, 500, etc.)
-   */
-  public ClientException(String message, Throwable cause, HttpStatus status) {
-    super(message, cause);
-    this.status = status;
-  }
-
-  /**
-   * Constructor.
-   *
-   * @param message Exception message.
-   * @param status  It's usually a HTTP Status Code (404, 500, etc.)
-   */
-  public ClientException(String message, HttpStatus status) {
-    super(message);
-    this.status = status;
-  }
-
-  /**
-   * Constructor.
-   *
-   * @param cause  An exception.
-   * @param status It's usually a HTTP Status Code (404, 500, etc.)
-   */
-  public ClientException(Throwable cause, HttpStatus status) {
-    super(cause);
-    this.status = status;
-  }
-
-  /**
-   * Get the exception's status. It's usually a HTTP Status Code (404, 500, etc.)
-   */
-  public HttpStatus getStatus() {
-    return this.status;
-  }
 }
 
 

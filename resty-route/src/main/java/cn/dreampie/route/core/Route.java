bed67f2ad584b6d90ed70e41af93e2bb3c08a18f
Dreampie <Dreampie@outlook.com>
2015-08-21 15:45:10 +0800

    add  json + form params

--- resty-route/src/main/java/cn/dreampie/route/core/Route.java
+++ resty-route/src/main/java/cn/dreampie/route/core/Route.java
@@ -650,6 +650,20 @@ public class Route {
         }
       }
     }
+
+    if (formParams.size() > 0) {
+      for (Map.Entry<String, List<String>> formEntry : formParams.entrySet()) {
+        name = formEntry.getKey();
+        if (!params.containsName(name)) {
+          valueArr = formEntry.getValue();
+          if (valueArr != null && valueArr.size() > 0) {
+            params.set(name, valueArr.get(0));
+          } else {
+            params.set(name, null);
+          }
+        }
+      }
+    }
     return params;
   }
 

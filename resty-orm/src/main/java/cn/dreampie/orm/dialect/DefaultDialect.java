d7f036346b6ac3da5421002d3a7261d666b45b47
Dreampie <Dreampie@outlook.com>
2015-10-10 11:44:47 +0800

    update single result for sql

--- resty-orm/src/main/java/cn/dreampie/orm/dialect/DefaultDialect.java
+++ resty-orm/src/main/java/cn/dreampie/orm/dialect/DefaultDialect.java
@@ -34,14 +34,17 @@ public abstract class DefaultDialect implements Dialect {
     put(Types.DOUBLE, Double.class);
     put(Types.DECIMAL, BigDecimal.class);
   }};
-  protected final Pattern selectPattern = Pattern.compile("^\\s*SELECT\\s*",
+  protected final Pattern selectPattern = Pattern.compile("^\\s*SELECT\\s+",
+      Pattern.CASE_INSENSITIVE);
+  protected final Pattern orderPattern = Pattern.compile("\\s+ORDER\\s+BY\\s+",
       Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-  protected final Pattern orderPattern = Pattern.compile("\\s*ORDER\\s*BY\\s*",
+  protected final Pattern groupPattern = Pattern.compile("\\s+GROUP\\s+BY\\s+",
       Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-  protected final Pattern groupPattern = Pattern.compile("\\s*GROUP\\s*BY\\s*",
+  protected final Pattern havingPattern = Pattern.compile("\\s+HAVING\\s+",
       Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-  protected final Pattern havingPattern = Pattern.compile("\\s*HAVING\\s*",
-      Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+
+  protected final Pattern selectSinglePattern = Pattern.compile("^\\s*SELECT\\s+((COUNT)\\([\\s\\S]*\\)\\s*,?)+(\\s*|\\s+(FROM[\\s\\S]*))?$",
+      Pattern.CASE_INSENSITIVE);
 
   public Class getColumnType(int type) {
     return typeMappings.get(type);
--- resty-orm/src/main/java/cn/dreampie/orm/dialect/MSSQLDialect.java
+++ resty-orm/src/main/java/cn/dreampie/orm/dialect/MSSQLDialect.java
@@ -20,6 +20,13 @@ public class MSSQLDialect extends DefaultDialect {
   }
 
   public String paginateWith(int pageNumber, int pageSize, String sql) {
+    if (pageNumber == 1 && pageSize == 1) {
+      //如果sql本身只返回一个结果
+      if (selectSinglePattern.matcher(sql).find()) {
+        return sql;
+      }
+    }
+
     int offset = pageSize * (pageNumber - 1);
 
     String orderBys = null;

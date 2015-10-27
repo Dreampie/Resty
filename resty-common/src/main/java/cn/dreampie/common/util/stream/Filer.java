89c080078a6c634991ed144bf161e5cdaaf9aa43
Dreampie <Dreampie@outlook.com>
2015-10-27 14:43:29 +0800

    add read file list

--- resty-common/src/main/java/cn/dreampie/common/util/stream/Filer.java
+++ resty-common/src/main/java/cn/dreampie/common/util/stream/Filer.java
@@ -3,7 +3,9 @@ package cn.dreampie.common.util.stream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 /**
  * Created by ice on 15-1-28.
@@ -40,4 +42,26 @@ public class Filer {
     return false;
   }
 
+  public static List<File> files(String dir) {
+    return files(new File(dir));
+  }
+
+  public static List<File> files(File dir) {
+    List<File> result = new ArrayList<File>();
+    if (dir.exists()) {
+      File[] files = dir.listFiles();
+      if (files != null && files.length > 0) {
+        for (File file : files) {
+          if (!file.isHidden()) {
+            if (file.isDirectory()) {
+              result.addAll(files(new File(file.getAbsolutePath())));
+            } else {
+              result.add(file);
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
 }

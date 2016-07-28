979d1bfc6ce94709288664ebb20373976737e161
Dreampie <Dreampie@outlook.com>
2016-07-28 09:58:20 +0800

    Update OpenID.java

--- resty-oauth/src/main/java/cn/dreampie/oauth/entity/OpenID.java
+++ resty-oauth/src/main/java/cn/dreampie/oauth/entity/OpenID.java
@@ -7,6 +7,6 @@ import cn.dreampie.orm.annotation.Table;
  * Created by Dreampie on 16/7/8.
  */
 @Table(name = "oau_openid", cached = true)
-public class Openid extends Model<Openid> {
-  public static final Openid DAO = new Openid();
+public class OpenID extends Model<OpenID> {
+  public static final OpenID DAO = new OpenID();
 }
